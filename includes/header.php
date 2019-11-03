f3ad27b1e1904beb2ab63ec1ba3d8717e24b0dd1
jayro <jayro1996.ag@gmail.com>
2019-11-03 14:04:05 -0500

    proyecto arquitectura

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP CRUD MYSQL</title>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+  <link rel="stylesheet" href="style.css">
+  <!-- BOOTSTRAP 4 -->
+  <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+  <!-- FONT AWESOEM -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+
+<body class="fondo">
+
+  <nav class="navbar navbar-dark  bg-primary ">
+    <div class="container">
+      <a class="navbar-brand " href="index.php">PROYECTO TAREAS</a>
+    </div>
+  </nav>
